aec94f7d509e51b2122782ac8da5545f44ffd08f
Ejhay <ejhayyygofredo@gmail.com>
2024-09-10 11:18:48 +0800

    added navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Link from 'next/link';
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-    </>
+    <header className={`${styles.headerContainer} grid grid-cols-2`} >
+      <div className='flex items-center'>
+        <h1 className={`${styles.logo} font-bold`}>Ejhay</h1>
+      </div>
+      <nav className={styles.nav}>
+        <Link href="/">Home</Link>
+        <Link href="/about">Services</Link>
+        <Link href="/about">Project</Link>
+        <Link href="/about">Contact</Link>
+        <Link href="/about">Hire Me</Link>
+      </nav>
+    </header>
 )}
 
 export default Navbar
