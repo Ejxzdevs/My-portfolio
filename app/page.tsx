88988b102082c90c726e7e639268a42d84f25beb
Ejhay <ejhayyygofredo@gmail.com>
2024-09-11 03:33:44 +0800

    modified  classname

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ProfileSection from "./components/ProfileSection";
 
 export default function Home() {
   return (
-      <>
+      <main className={`mainContainer pb-8 `}>
         <ProfileSection/>
-      </>
+      </main>
   );
 }
