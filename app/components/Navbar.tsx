3a79e9a11bc6e292c00308877522c5ccace1600a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-09-13 01:50:24 +0800

    modified classname

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
   return (
     <header className={`${styles.headerContainer} grid grid-cols-2 lg:grid lg:grid-cols-2`} >
       <div className='flex items-center'>
-        <Link href="#Z" className={`${styles.logo} font-bold hidden sm:flex lg:flex`}>Ejhay</Link>
+        <Link href="#home" className={`${styles.logo} font-bold hidden sm:flex lg:flex`}>Ejhay</Link>
       </div>
       <nav className={`${styles.nav}`} >
-        <Link href="#B" className={`${styles.linkStyle} hidden sm:flex lg:flex`}>About</Link>
-        <Link href="#C" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Skills</Link>
-        <Link href="#D" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Projects</Link>
-        <Link href="#E" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Contact</Link>
+        <Link href="#about" className={`${styles.linkStyle} hidden sm:flex lg:flex`}>About</Link>
+        <Link href="#skills" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Skills</Link>
+        <Link href="#projects" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Projects</Link>
+        <Link href="#contact" className={`${styles.linkStyle} hidden sm:flex lg:flex `}>Contact</Link>
       </nav>
     </header>
 )}
